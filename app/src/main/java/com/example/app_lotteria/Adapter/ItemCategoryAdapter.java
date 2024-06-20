074364fec349f75c83623387da0acbb7b61eacdd
HuyEndy <nhokvip004@gmail.com>
2024-06-20 22:39:22 +0700

    fragment order

---
+++ app/src/main/java/com/example/app_lotteria/Adapter/ItemCategoryAdapter.java
@@ -0,0 +1,57 @@
+package com.example.app_lotteria.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.app_lotteria.Domain.CategoryDomain;
+import com.example.app_lotteria.databinding.ItemCategoryBinding;
+
+import java.util.ArrayList;
+
+public class ItemCategoryAdapter extends RecyclerView.Adapter<ItemCategoryAdapter.Viewholder> {
+
+    ArrayList<CategoryDomain> list;
+    private Context context;
+
+    public ItemCategoryAdapter(ArrayList<CategoryDomain> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ItemCategoryAdapter.Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        ItemCategoryBinding binding = ItemCategoryBinding.inflate(LayoutInflater.from(context), parent, false);
+        return new Viewholder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemCategoryAdapter.Viewholder holder, int position) {
+        holder.binding.titleCategory.setText(list.get(position).getTitle());
+
+        Glide.with(context)
+                .load(list.get(position).getPicUrl())
+                .into(holder.binding.iconCategory);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class Viewholder extends RecyclerView.ViewHolder {
+        ItemCategoryBinding binding;
+
+        public Viewholder(ItemCategoryBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+
+}
--- app/src/main/java/com/example/app_lotteria/Fragment/OrderFragment.java
+++ app/src/main/java/com/example/app_lotteria/Fragment/OrderFragment.java
@@ -2,18 +2,35 @@ package com.example.app_lotteria.Fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.app_lotteria.Adapter.CategoryAdapter;
+import com.example.app_lotteria.Adapter.ItemCategoryAdapter;
+import com.example.app_lotteria.Domain.CategoryDomain;
 import com.example.app_lotteria.R;
+import com.example.app_lotteria.databinding.FragmentHomeBinding;
+import com.example.app_lotteria.databinding.FragmentOrderBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 
 public class OrderFragment extends Fragment {
 
+    private FragmentOrderBinding binding;
 
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
 
     public OrderFragment() {
         // Required empty public constructor
@@ -23,6 +40,55 @@ public class OrderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order, container, false);
+        binding = FragmentOrderBinding.inflate(inflater,container,false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        initCategory();
+    }
+
+    private void initCategory() {
+        DatabaseReference myRef = database.getReference("Category");
+
+        ArrayList<CategoryDomain> list = new ArrayList<>();
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    for (DataSnapshot data:snapshot.getChildren()) {
+                        list.add(data.getValue(CategoryDomain.class));
+                    }
+
+                    if (!list.isEmpty()){
+                        binding.recyclerViewCategory.setLayoutManager(new GridLayoutManager(getContext(),2));
+
+                        binding.recyclerViewCategory.setAdapter(new ItemCategoryAdapter(list));
+
+                    }
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
